313337a203412bb3c577bef4dbc6973f20b37e9b
vivekj52 <vivekj52@gmail.com>
2020-12-25 20:07:42 +0530

    Login Form changes

--- authorization/urls.py
+++ authorization/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import SignUpView
 
-from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
