bda0393c9f3209456d601301e5d3d3907dbc9de5
vivekj52 <vivekj52@gmail.com>
2020-12-25 22:09:39 +0530

    Sigup page modified

--- authorization/urls.py
+++ authorization/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import SignUpView
+from . import views
 
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('signup/', views.signuppage, name='signup')
 ]
--- authorization/views.py
+++ authorization/views.py
@@ -1,10 +1,26 @@
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
-# Create your views here.
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+def signuppage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'registration/signup.html', context)
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
